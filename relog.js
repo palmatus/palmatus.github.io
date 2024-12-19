6605a38df34b4728e71d1c673e4994127f49aeb6
Palma Tus <154108693+palmatus@users.noreply.github.com>
2024-12-19 13:14:07 -0600

    Update relog.js

--- relog.js
+++ relog.js
@@ -74,15 +74,18 @@ function año() {
     document.getElementById('año').innerHTML = year
 }
 
-// Tabular
+// event listener for keyup
 function checkTabPress(e) {
     "use strict";
-
+    // pick passed event of global event object
     e = e || event;
     var activeElement;
-    if (e.keyCode == 9) {.
+    if (e.keyCode == 9) {
+        // Here read the active selected link.
         activeElement = document.activeElement;
+        // If HTML element is and anchor
         if (activeElement.tagName.toLowerCase() == 'a')
+            // get it's hyperlink
             alert(activeElement.href);
     }
 }
